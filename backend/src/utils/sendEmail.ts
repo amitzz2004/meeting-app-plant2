1a9f34d74fe8ab3000346520807f9cb279c678b3
Amitzz2004 <amitchhotaray19@gmail.com>
2026-04-27 09:53:30 +0530

    mail changed to outlook

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -1,10 +1,16 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.office365.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    ciphers: "SSLv3",
+    rejectUnauthorized: false
   }
 });
 
